22612fa8b5cf1650fda257c662962036c4cf2725
UWENAYO Alain Pacifique <uwenayoallain@gmail.com>
2022-05-12 21:43:16 +0200

    making a canvas

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import './style.css'
 import * as THREE from 'three';
 
 const scene = new THREE.Scene()
-
+const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGL1Renderer({
-    canvas: document.createElement('canvas'),
+    canvas: canvas as HTMLCanvasElement
 });
 
 renderer.setPixelRatio(window.devicePixelRatio);
