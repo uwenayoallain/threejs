bf7213b91431700b2794f27011f9fe07b4a1384c
UWENAYO Alain Pacifique <uwenayoallain@gmail.com>
2022-05-12 21:36:49 +0200

    rendering my first webgli component

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,13 @@ import * as THREE from 'three';
 
 const scene = new THREE.Scene()
 
-const camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight, 0.1,1000);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGL1Renderer({
+    canvas: document.createElement('canvas'),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.z = 30;
+renderer.render(scene, camera)
