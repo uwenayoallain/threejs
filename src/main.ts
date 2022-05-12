d866aa5d9b2340cb23bf8dd63f0b8a8167b52c94
UWENAYO Alain Pacifique <uwenayoallain@gmail.com>
2022-05-12 21:30:56 +0200

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
 import './style.css'
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+import * as THREE from 'three';
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const scene = new THREE.Scene()
+
+const camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight, 0.1,1000);
