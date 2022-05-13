2e4b259060c7085af3e6757411a867e624b87095
UWENAYO Alain Pacifique <uwenayoallain@gmail.com>
2022-05-13 10:43:02 +0200

    star geometry

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import './style.css'
 
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 const scene = new THREE.Scene()
 const canvas = document.getElementById('canvas') as HTMLCanvasElement;
@@ -16,6 +17,15 @@ renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(50);
 renderer.render(scene, camera)
+const controls = new OrbitControls(camera, renderer.domElement);
+function addStar() {
+    const geometry = new THREE.SphereGeometry(0.5, 32, 32);
+    const material = new THREE.MeshBasicMaterial({
+        color: 0xffffff
+    });
+    const star = new THREE.Mesh(geometry, material);
+    scene.add(star);
+}
 
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
 const material = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
@@ -29,11 +39,12 @@ scene.add(pointLight, ambientLight);
 const gridHelper = new THREE.GridHelper(200, 50);
 const lightHelper = new THREE.PointLightHelper(pointLight);
 scene.add(lightHelper, gridHelper);
-
+addStar();
 function animate() {
     requestAnimationFrame(animate);
     torus.rotation.x += 0.01;
     torus.rotation.y += 0.01;
+    controls.update();
     renderer.render(scene, camera);
 }
 animate();
